b1546646f95124b231163118ce058a6cf5f9b7f8
PCMI-CASTILHO <pcmipesoexatobalancas@gmail.com>
2026-02-18 08:49:30 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@
 importScripts('https://cdn.jsdelivr.net/npm/idb@8/build/umd.js');
 
 // Nomenclatura de cache versionada
-const CACHE_NAME = 'formulario-cache-v0111';
+const CACHE_NAME = 'formulario-cache-v0112';
 
 // Assets críticos para instalação mínima
 const CORE_ASSETS = [
